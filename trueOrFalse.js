a4663a2161683590c652d8155430588068465943
sheikhmohibulhasan <sheikhhasan24@gmail.com>
2020-03-06 00:07:59 +0600

    nullvs undefined fully described

---
+++ trueOrFalse.js
@@ -0,0 +1,21 @@
+// const name = "";
+let name = 0;
+// console.log(name)
+if(name || name == 0){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is false");
+}
+
+/* False values:
+	1. 0
+	2. ""
+	3. undefined
+	4. null
+	5. NaN
+	6. false
+
+	Truthy values:
+	1. "0", " ", [], {}
+	2. 
